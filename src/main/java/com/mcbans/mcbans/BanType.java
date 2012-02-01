d5ce33272615cbdd03fa16122a6853c8348110a6
Sean Gordon <sean_aldon@hotmail.co.uk>
2012-02-01 13:36:01 +0000

    Much improved BanType enum

--- src/main/java/com/mcbans/mcbans/BanType.java
+++ src/main/java/com/mcbans/mcbans/BanType.java
@@ -1,12 +1,48 @@
 package com.mcbans.mcbans;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Sean
  */
 public enum BanType {
-	GLOBAL,
-	LOCAL,
-	TEMP,
-	UNBAN;
+
+	GLOBAL_BAN ('g', "globalBan", "mcbans.ban.global"),
+	LOCAL_BAN ('l', "localBan",  "mcbans.ban.local"),
+	TEMP_BAN ('t', "tempBan",  "mcbans.ban.temp"),
+	UNBAN ('u', "unBan",  "mcbans.ban.unban");
+
+	private final char code;
+	private final String apiAction;
+	private final String permission;
+	private static final HashMap<Character, BanType> flags = new HashMap<Character, BanType>();
+
+	private BanType(char code, String apiAction, String permission) {
+		this.code = code;
+		this.apiAction = apiAction;
+		this.permission = permission;
+	}
+
+	public char getChar() {
+		return code;
+	}
+
+	public String getPermission() {
+		return permission;
+	}
+
+	public String getAPICode() {
+		return apiAction;
+	}
+
+	public static BanType getByChar(char code) {
+		return flags.get(code);
+	}
+
+    static {
+        for (BanType flag : BanType.values()) {
+            flags.put(flag.getChar(), flag);
+        }
+    }
 }
