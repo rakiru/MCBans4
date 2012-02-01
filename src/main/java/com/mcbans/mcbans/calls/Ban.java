f8d970ae2944aeb381c74863dd03ff022fc2a4e6
Sean Gordon <sean_aldon@hotmail.co.uk>
2012-02-01 13:29:51 +0000

    Most of the framework for banning

--- src/main/java/com/mcbans/mcbans/calls/Ban.java
+++ src/main/java/com/mcbans/mcbans/calls/Ban.java
@@ -1,10 +1,16 @@
 package com.mcbans.mcbans.calls;
 
+import com.mcbans.mcbans.BanType;
 import com.mcbans.mcbans.MCBansPlugin;
 import com.mcbans.mcbans.utils.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.entity.Player;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -12,27 +18,93 @@ import org.bukkit.entity.Player;
  */
 public class Ban implements Runnable {
 
-	protected String playerName;
-	protected final MCBansPlugin plugin;
-	protected final String adminName;
-	protected final String kickReason;
+	private String playerName;
+	private final MCBansPlugin plugin;
+	private final String adminName;
+	private String reason;
+	private BanType banType;
+	private String playerIP;
+	private String time;
+	private String timeUnit;
+	private int handlersLeft;
+	public final long timeAdded;
 
-	public Ban(MCBansPlugin plugin, String playerName, String adminName, String kickReason) {
+	public Ban(MCBansPlugin plugin, String playerName, String adminName, String reason, BanType banType, String playerIP, String time, String timeUnit, int handlers) {
 		this.plugin = plugin;
 		this.playerName = playerName;
 		this.adminName = adminName;
-		this.kickReason = kickReason;
+		this.reason = reason;
+		this.handlersLeft = handlers;
+		this.banType = banType;
+		this.playerIP = playerIP;
+		this.time = time;
+		this.timeUnit = timeUnit;
+		timeAdded = System.currentTimeMillis();
+	}
+
+	public synchronized void appendReason(String reasonEnd) {
+		reason += ' ' + reasonEnd;
+	}
+
+	public synchronized void finishHandler() {
+		handlersLeft--;
 	}
 
 	public void run() {
+		//Allow max of 3 seconds for handlers to do their stuff before continuing (add this as a config option at some point)
+		if (handlersLeft > 0 || timeAdded - System.currentTimeMillis() < 3000) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(Ban.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
 		Player player;
 		try {
+			//TODO: Tdiy this up - seriously
 			player = PlayerMethods.getPlayer(plugin, playerName);
 			playerName = PlayerMethods.getPlayerName(plugin, player);
-			PlayerMethods.kickPlayer(plugin, player, plugin.lang.getFormat("kickMessagePlayer", playerName, adminName, kickReason));
-			PlayerMethods.broadcast(plugin, plugin.lang.getFormat("kickMessageSuccess", playerName, adminName, kickReason));
+			//Player may have rejoined in the time taken for the handlers to do their stuff
+			PlayerMethods.kickPlayer(plugin, player, plugin.lang.getFormat("kickMessagePlayer", playerName, adminName, reason));
+			ban();
 		} catch (InterruptedException e) {
 			plugin.log.debug(e.toString());
+		}
+	}
+
+	private void ban() {
+		HashMap<String, String> urlItems = new HashMap<String, String>();
+		JSONHandler webHandler = new JSONHandler(plugin);
+		urlItems.put("player", playerName);
+		urlItems.put("playerip", playerIP);
+		urlItems.put("reason", reason);
+		urlItems.put("admin", adminName);
+		urlItems.put("exec", banType.getAPICode());
+		HashMap<String, String> result = webHandler.mainRequest(urlItems);
+		try {
+			//An error happened
+			if (!result.containsKey("result")) {
+				//TODO: We should probably handle this...
+			}
+			//Several possible response codes
+			if (result.get("result").equals("y")) {
+				//Success!
+
+			} else if (result.get("result").equals("e")) {
+				//Error :(
+
+			} else if (result.get("result").equals("w")) {
+				//Warning
+
+			} else if (result.get("result").equals("s")) {
+				//Something
+
+			} else if (result.get("result").equals("a")) {
+				//Already banned
+
+			}
+		} catch (NullPointerException ex) {
+			plugin.log.debug(ex.toString());
 		}
 	}
 }
--- src/main/java/com/mcbans/mcbans/commands/BanCommand.java
+++ src/main/java/com/mcbans/mcbans/commands/BanCommand.java
@@ -1,9 +1,17 @@
 package com.mcbans.mcbans.commands;
 
+import com.mcbans.mcbans.BanType;
+import com.mcbans.mcbans.MCBansAPI;
 import com.mcbans.mcbans.MCBansPlugin;
+import com.mcbans.mcbans.api.FlagHandler;
+import com.mcbans.mcbans.api.MCBansPluginManager;
+import com.mcbans.mcbans.calls.Ban;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -11,21 +19,121 @@ import org.bukkit.command.CommandSender;
  */
 public class BanCommand implements CommandExecutor {
 
-	private final MCBansPlugin MCBans;
+	private final MCBansPlugin plugin;
+	private final MCBansPluginManager manager;
+	private final Pattern validUsername = Pattern.compile(MCBansAPI.VALID_PLAYER_NAME_REGEX);
 
 	public BanCommand(MCBansPlugin plugin) {
-		this.MCBans = plugin;
+		this.plugin = plugin;
+		this.manager = MCBansAPI.getInstance().getPluginManager();
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] split) {
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!sender.hasPermission("mcbans.ban")) {
-			sender.sendMessage(MCBans.lang.getFormat("permissionDenied"));
+			sender.sendMessage(plugin.lang.getFormat("permissionDenied"));
 			return true;
 		}
 
-		//Ban ban = new Ban();
-		//ban.start();
+		String adminName = sender.getName();
+		String banReason = null;
+		String playerName = null;
+		String flagString = null;
+		String playerIP = "";
+		String time = null;
+		String timeUnit = null;
+		BanType banType = BanType.LOCAL_BAN;
+		ArrayList<FlagHandler> customFlags = new ArrayList<FlagHandler>();
+
+		if (args.length == 0) {
+			//No arguments - show command usage
+			return false;
+		}
+
+		switch (args.length) {
+			case 0:
+				//No arguments - show command usage
+				return false;
+			case 1:
+				//One argument - player to be banned (only valid case)
+				if (args[0].startsWith("-")) {
+					//Flags used but not other info (player name, etc) - show command usage
+					return false;
+				} else {
+					playerName = args[0];
+					banReason = plugin.config.getDefaultLocalBanReason();
+				}
+				break;
+			default:
+				//More than one argument - player to be banned and reason, or player to be banned and flags
+				if (args[0].startsWith("-")) {
+					//Flags used - parse them and second arg must be player to be banned
+					flagString = args[0].substring(1);
+					char[] flagChars = flagString.toCharArray();
+					for (char flag : flagChars) {
+						BanType f = BanType.getByChar(flag);
+						if (f == null) {
+							FlagHandler h = manager.getHandler(flag);
+							if (h == null) {
+								//TODO: Message player - invalid flag entered
+							} else {
+								customFlags.add(h);
+							}
+						} else {
+							banType = f;
+						}
+					}
+					playerName = args[1];
+					int reasonStart;
+					if (banType == BanType.TEMP_BAN && args.length >= 4) {
+						//TODO: Should either add a default time in config or error here if no time entered
+						time = args[2];
+						timeUnit = args[3];
+						reasonStart = 4;
+					} else {
+						reasonStart = 2;
+					}
+					if (args.length > reasonStart) {
+						StringBuilder sb = new StringBuilder(args[reasonStart]);
+						for (int i = reasonStart; i < args.length; i++) {
+							sb.append(' ');
+							sb.append(args[i]);
+						}
+						banReason = sb.toString();
+					}
+				} else {
+					//Player name and ban reason - local ban player
+					playerName = args[0];
+					StringBuilder sb = new StringBuilder(args[1]);
+					for (int i = 2; i < args.length; i++) {
+						sb.append(' ');
+						sb.append(args[i]);
+					}
+					banReason = sb.toString();
+				}
+				break;
+		}
+
+		//Check flag permissions
+		if (plugin.checkPermission(sender, banType.getPermission())) {
+			return true;
+		}
+		//Check for global ban without reason
+		if (banType == BanType.GLOBAL_BAN && banReason == null) {
+			//TODO: Message player
+		}
+
+		Player player = plugin.getServer().getPlayerExact(playerName);
+		if (player != null) {
+			playerIP = player.getAddress().toString();
+		}
+
+		Ban ban = new Ban(plugin, playerName, adminName, banReason, banType, playerIP, time, timeUnit, customFlags.size());
+		//Run custom flag handlers
+		for (FlagHandler h : customFlags) {
+			h.run(playerName, adminName, banReason, flagString, args);
+		}
+
 		return true;
 	}
 }
